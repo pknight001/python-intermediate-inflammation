7a487b4ffc9a1c7a0a30f746b68591f917812421
Knight, Peter J <peter.knight@ukaea.uk>
2023-04-12 16:31:35 +0100

    Added patient normalize function + test

--- inflammation/models.py
+++ inflammation/models.py
@@ -43,3 +43,9 @@ def daily_min(data):
     :returns: An array of minimum values of measurements for each day.
     """
     return np.min(data, axis=0)
+
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array."""
+    max_data = np.max(data, axis=1)
+    return data / max_data[:, np.newaxis]
